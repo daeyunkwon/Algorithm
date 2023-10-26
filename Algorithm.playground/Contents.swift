a52302d43f4c86aa1c08d76a36158dae93facb11
Daeyun <sweet_ray@naver.com>
2023-10-26 14:53:00 +0900

    백준 폴더 생성

--- Algorithm.playground/Contents.swift
+++
@@ -1,8 +0,0 @@
-import UIKit
-
-var greeting = "Hello, playground"
-
-print(greeting)
-
-
-print(greeting)
---
+++ BackJoon/1000.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  Coding
+//
+//  Created by 권대윤 on 10/26/23.
+//
+
+import Foundation
+
+
+let line = readLine()!
+
+let array = line.components(separatedBy: " ")
+
+let a = Int(array[0])!
+let b = Int(array[1])!
+
+print(a + b)
+
