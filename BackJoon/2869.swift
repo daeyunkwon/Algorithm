4eaa7abd896498bdc4539575a7b47ef073229402
Daeyun <sweet_ray@naver.com>
2024-03-11 11:16:06 +0900

    add

---
+++ BackJoon/2869.swift
@@ -0,0 +1,14 @@
+
+
+import Foundation
+
+let input = readLine()!.split(separator: " ").map{Double($0)!}
+
+let a = input[0]
+let b = input[1]
+let v = input[2]
+
+
+let day = ceil((v - b) / (a - b))
+
+print(Int(day))
